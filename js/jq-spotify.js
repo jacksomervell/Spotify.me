9bf6a400efb8639f3c3d1f590ad6b7a8449b4243
Jack Somervell <jacksomervell@gmail.com>
2015-09-07 19:26:39 +0100

    sorted the displaying of the search results just need to style

--- js/jq-spotify.js
+++ js/jq-spotify.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
   
   var searchQuery =  $('#searcher')
   var searchType =  $('#select-type')
+  var results = $(".results");
 
   getMusicButton.on('click', getMusic);
 
   function getMusic(e) {
     $.get('https://api.spotify.com/v1/search?q=' + searchQuery.val() + '&type=' + searchType.val(), function(response){
-      console.log(response)
-    }
-  )}
+      results.empty()
     
+      var alteredType = searchType.val() + "s"
 
-});
+      $.each(response[alteredType].items, function(index, alteredType) {
+            var result = $("<div class = 'results'>" + alteredType.name + "</div>");
+            results.append(result);
+          });
+        })
+      }
+    });
