111a2f55363bbef0a671b16724fe190f766b88da
Jack Somervell <jacksomervell@gmail.com>
2015-09-08 10:17:53 +0100

    added change dynamic

--- js/jq-spotify.js
+++ js/jq-spotify.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
   var results = $(".results");
 
   getMusicButton.on('click', getMusic);
+  searchType.on('change', getMusic);
 
   function getMusic(e) {
     $.get('https://api.spotify.com/v1/search?q=' + searchQuery.val() + '&type=' + searchType.val(), function(response){
@@ -23,8 +24,8 @@ $(document).ready(function() {
       if (alteredType === "tracks") {
         console.log("tracktime");
       $.each(response[alteredType].items, function(index, alteredType){
-        var preview = $("<div class = 'results'>" + alteredType.preview_url + "</div>");
-            results.append(preview);
+        var preview = $( "<audio class = 'previewurl' src=" + alteredType.preview_url + "></audio>");
+            $(".previewurl").append(preview);
         })
       }    
     })
